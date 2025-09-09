9963237ad01e5e4e6a4750587212e7b44b63e160
CHEASENGHOUR <cheasenghour789@gmail.com>
2025-09-09 12:21:21 +0700

    delete data

--- api/delete_product.php
+++ api/delete_product.php
@@ -5,6 +5,21 @@ include("db.php");
 header("Content-Type: application/json");
 $id = $_POST["id"];
 
+$query = "SELECT product_img FROM products WHERE id = ?";
+$result = $conn->prepare($query);
+$result->bind_param("i", $id);
+$result->execute();
+$result->bind_result($image);
+$result->fetch();
+$result->close();
+
+if($image){
+    $dir = __DIR__ . "/product_image/" . $image;
+    if(file_exists($dir)){
+        unlink($dir);
+    }
+}
+
 $query = "DELETE FROM products WHERE id = ? ;";
 $stmt = $conn->prepare($query);
 $stmt->bind_param("i", $id);
